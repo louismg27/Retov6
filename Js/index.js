eef25cf0a918570c2a70f797e83eaac04a350d9a
louismg27 <louismg27@gmail.com>
2022-05-11 17:44:25 -0500

    reto6_header.v.1

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,10 @@
 'use strict';
 import {  readProduct,calculate,trash} from "./AddCartShopping.js";
 import {compCarShopping} from "./compCarShopping.js";
+import header from "./header.js";
 
 function DocReady() {
+    header();
     compCarShopping();
     readProduct();
     calculate();
